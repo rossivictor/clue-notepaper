c857602e19c5c201f093d922fcfdc64325a0d7ba
Victor Rossi <rossivictor90@gmail.com>
2025-01-14 20:03:16 -0300

    implement Google Tag Manager

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import TagManager from "react-gtm-module";
 
-createRoot(document.getElementById('root')!).render(
+const tagManagerArgs = {
+  gtmId: "GTM-P6D8QLVG",
+};
+
+TagManager.initialize(tagManagerArgs);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
